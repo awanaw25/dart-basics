0a70df74048817113985a2800cee25c5629a5f83
awanaw25 <putrakembang47@gmail.com>
2024-03-10 22:24:48 +0700

    latihan2_tugas3

---
+++ dart_project/bin/latihan2/tugas3.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+void main() {
+  stdout.write("Masukkan angka untuk menghitung faktorial: ");
+  int angka = int.parse(stdin.readLineSync()!);
+
+  int faktorial = 1;
+
+  int i = 1;
+  while (i <= angka) {
+    faktorial *= i;
+    i++;
+  }
+
+  print("Faktorial dari $angka adalah: $faktorial");
+}
