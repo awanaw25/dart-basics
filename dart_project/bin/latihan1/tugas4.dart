b031794dff7d91425706aed757b883056bb50ccb
awanaw25 <putrakembang47@gmail.com>
2024-03-10 21:01:09 +0700

    tugas4

---
+++ dart_project/bin/latihan1/tugas4.dart
@@ -0,0 +1,9 @@
+void main() {
+  double prinsipal = 1000;
+  int waktu = 2;
+  double tingkatBunga = 5;
+
+  double bunga = (prinsipal * waktu * tingkatBunga) / 100;
+
+  print("Bunga Sederhana Adalah: $bunga");
+}
