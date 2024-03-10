9cef0235681df2b06a3276c0f747db2f1a645162
awanaw25 <putrakembang47@gmail.com>
2024-03-10 22:32:51 +0700

    latihan2_tugas10

---
+++ dart_project/bin/latihan2/tugas10.dart
@@ -0,0 +1,7 @@
+void main() {
+  List<String> daftarKata = ['apel', 'pisang', 'jeruk', 'anggur', 'mangga'];
+
+  for (String kata in daftarKata) {
+    print('Kata: $kata, Panjang: ${kata.length}');
+  }
+}
